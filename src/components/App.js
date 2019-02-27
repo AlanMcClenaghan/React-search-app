fcc040ed3634f0567571c9f1d0b96a8d64b3fadf
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-02-27 12:06:26 +0000

    Making API Requests in React using axios.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 const style = { marginTop: '10px' }
 
 class App extends React.Component {
 
-  onSearchSubmit(searchTerm) {
-    console.log(searchTerm)
+  state = {
+    images: []
+  };
+
+  onSearchSubmit = async searchTerm => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: searchTerm },
+    })
+
+    this.setState({ images: response.data.results });
+    console.log(this.state.images);
+
+    // // Using .then at the end of the promise
+    // .then(response => {
+    //   console.log(response.data.results);
+    // });
   }
 
   render() {
     return (
       <div className="ui container" style={style} >
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
