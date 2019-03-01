991ed2ddd247d382279e837e058c8c2ce15d6707
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-03-01 20:48:04 +0000

    Using Refs for DOM Access and used CSS Grid.

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
-  const images = props.images.map(({ urls, description, id }) => {
-    return <img src={urls.regular} alt={description} key={id} />
+  const images = props.images.map((image) => {
+    return <ImageCard image={image} key={image.id} />
   })
   return (
-    <div>
+    <div className="image-list">
       {images}
     </div>
   )
